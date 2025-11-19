65c0afa518b28904f289302e5a9db79ebfdca2db
Emanueli Cristina <cristinagodoy89br@gmail.com>
2025-11-19 18:58:38 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  
+  let excuse = whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+
+  
+  document.getElementById("excuse").innerHTML = excuse;
+
+
+
+
+
+
+
   console.log("Hello Rigo from the console!");
 };
+
+
+
